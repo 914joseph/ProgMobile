5d8c53ed3bba3c8010258ed6396ff3c1e2707247
evic08 <103127737+evic08@users.noreply.github.com>
2022-07-26 11:05:24 -0300

    Create BD.dart
    esboço do banco, não testei com o resto do código

---
+++ BD.dart
@@ -0,0 +1,30 @@
+import 'package:testarpmobile/dados_usuario_card.dart';
+
+class BD {
+  static List<DadosUsuarioCard> listaUsuario = [
+    DadosUsuarioCard(
+      nome:'Bianca Laiany Bastos Rulim Nunes',
+      cpf: '000.000.000-00',
+      entrada: '26/07/2022',
+      situacao: 'ativo',
+    ),
+    DadosUsuarioCard(
+      nome:'Emylle Victória Cavalcante Costa',
+      cpf: '111.111.111-11',
+      entrada: '26/07/2022',
+      situacao: 'ativo',
+    ),
+    DadosUsuarioCard(
+      nome:'Josenilton Ferreira Júnior',
+      cpf: '222.222.222-22',
+      entrada: '26/07/2022',
+      situacao: 'ativo',
+    ),
+    DadosUsuarioCard(
+      nome:'Thayná Ingrid de Paula',
+      cpf: '333.333.333-33',
+      entrada: '26/07/2022',
+      situacao: 'ativo',
+    ),
+  ];
+}
